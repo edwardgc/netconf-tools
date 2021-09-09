78208673e43eb96b37b5d6c3b9dcfc8080f57c12
edwardgc <edwardgc@gmail.com>
2021-09-09 10:47:16 +0800

    add build.xml

---
+++ src/performance/DeviationMain.java
@@ -0,0 +1,14 @@
+package performance;
+
+import java.io.File;
+
+public class DeviationMain {
+
+    private static String YANG_DIR = "yang";
+
+    public static void main(String args[]) throws Exception {
+        File file = new File(".");
+        System.out.println(file.getAbsolutePath());
+    }
+
+}
